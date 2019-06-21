d68a120e617aaf2cdae088d2e8a6af561b7f3067
4 <4>
2019-06-21 08:59:53 +0800

    6/21/9：00提交

--- src/pages/coupon/children/pustDue.vue
+++ src/pages/coupon/children/pustDue.vue
@@ -1,6 +1,15 @@
 <template>
     <div id="pust" class="page">
         <h1>已过期</h1>
+        <div class="pust-box">
+            <van-card
+                num="2"
+                price="2.00"
+                desc="描述信息"  
+                title="商品标题"
+                :thumb="imageURL"
+                />
+        </div>
     </div>
 </template>
 
@@ -10,6 +19,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    #pust{
+        width: 100%;
+        min-height: 100%;
+        background: red;
+    }
 </style>
