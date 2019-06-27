27e5038075af562bd8af2301eca4914cbc05cec7
Bitter <2474069756@qq.com>
2019-06-27 16:07:00 +0800

    收藏调用接口

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Swipe, SwipeItem, Toast,NavBar,Icon,Card,Tab, Tabs,Button,Cell, CellGroup  } from 'vant'
+import { Swipe, SwipeItem, Toast,NavBar,Icon,Card,Tab, Tabs,Button,Cell, CellGroup,Dialog} from 'vant'
 import 'lib-flexible/flexible'
 import { SwipeCell } from 'vant';
+import animated from 'animate.css' // npm install animate.css --save安装，再引入
 
-Vue.use(Icon).use(Button).use(Card).use(NavBar).use(SwipeCell).use(Tab).use(Tabs).use(Swipe).use(SwipeItem).use(Toast).use(Cell).use(CellGroup);
+Vue.use(animated)
+Vue.use(Icon).use(Dialog).use(Button).use(Card).use(NavBar).use(SwipeCell).use(Tab).use(Tabs).use(Swipe).use(SwipeItem).use(Toast).use(Cell).use(CellGroup);
 
 
 Vue.config.productionTip = false
--- src/pages/collect/children/commodity.vue
+++ src/pages/collect/children/commodity.vue
@@ -2,47 +2,200 @@
 <div id="commodity">
 
 
-        <van-swipe-cell :right-width="60" :left-width="0" :on-close="onClose">
-         
-        <van-cell class="vanList">
-            <p>123</p>
-        </van-cell>
-        <van-button
-            class="danger"
-            square
-            slot="right"
-            type="danger"
-            text="删除"
-           
-        />
+       <van-swipe-cell :right-width="60" :left-width="0" :on-close="onClose"
+          v-for="(item,index) in vanCellList" 
+          :key="index"
+          :index="item.productId">
+            
+            <van-cell class="vanList">
+                <div class="shop">
+                    <p class="logo">
+                      <img :src="item.imagePath" alt="">
+                    </p>
+                    <div class="massage">
+                      <p>{{item.productName}}</p>
+                      <a>¥{{item.minSalePrice}}</a>
+                      <span>{{item.evaluation}}人评价</span>
+                    </div>
+                </div>
+            </van-cell>
+            <van-button
+                class="danger"
+                square
+                slot="right"
+                type="danger"
+                text="删除"
+                @click="dangerAction(item.productId)"
+            />
         </van-swipe-cell>
+
+        <transition name="slide-fade" enter-active-class=" animated fadeIn" leave-active-class=" animated fadeOut" >
+          <div role="dialog" class="van-dialog" v-if="showmenu"   aria-labelledby="确定删除吗？" >
+            <div class="van-dialog__content">
+                <div class="van-dialog__message">
+                  确定删除吗？
+                </div>
+            </div>
+            <div class="van-hairline--top van-dialog__footer van-dialog__footer--buttons"  >
+              <button @click="cancelAction" class="van-button van-button--default van-button--large van-dialog__cancel">
+                <span class="van-button__text">取消</span>
+              </button>
+              <button @click="deleteAction" class="van-button  van-button--default van-button--large van-dialog__confirm van-hairline--left">
+                <span class="van-button__text">确认</span>
+              </button>
+            </div>
+          </div>
+        </transition>
+
+        <transition name="slide-fade" enter-active-class=" animated fadeIn" leave-active-class="animated fadeOut" >
+          <div class="cover" v-if="show"></div>
+        </transition>
 </div>
 </template>
 
 <script>
 import { constants } from 'crypto';
+import Axios from 'axios';
+import { setTimeout } from 'timers';
 
 export default {
-  methods: {
-    onClose() {
-       console.log
+  name:'',
+  data(){
+    return{
+        vanCellList:[],
+        show:false,
+        showmenu:false,
     }
   },
-  dangerAction(){
-      console.log(1223)
-  }
+  methods: {
+    getShopData(){
+          Axios.get("http://192.168.1.24:8080/gateway/mobileMemberCenterService/memberCenter/getUserCollectionProduct",{
+            headers:{
+               Authorization:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzY1MSIsImlhdCI6MTU2MTUzNjAxOSwiZXhwIjoxNTYxNjIyNDE5fQ.q1XeH8t3E6GcZqeCyQ3NNL4drXn6rXMydoAz7vOqMbH4vePQu42i_rKJXrpEM0lEsezoDGExveMlfy8rwUA1aA"
+            }
+          }).then(res=>{
+            this.vanCellList = res.data.data.map(item=>{
+                return{
+                evaluation:item.evaluation,
+                imagePath:item.imagePath,
+                minSalePrice:item.minSalePrice,
+                productName:item.productName,
+                productId:item.productId,
+              }
+            })
+            // this.vanCellList = res.data.data
+            console.log(this.vanCellList)
+          })
+      },
+    onClose() {
+         
+    },
+    dangerAction(index){
+        console.log("确定删除商品吗")
+        console.log(index)
+        sessionStorage.setItem("index",index)
+        this.showmenu = !this.showmenu;
+        this.show = !this.show;
+    },
+    deleteAction(index){
+             console.log("确认删除")
+             let ProductId = sessionStorage.index
+             console.log(ProductId)
+            //  this.vanCellList.splice(index,1);
+             this.showmenu = !this.showmenu;
+             this.show = !this.show;
+             console.log(this.vanCellList)
+
+    },
+    cancelAction(){
+        console.log("取消删除")
+        this.showmenu = !this.showmenu;
+        this.show = !this.show;
+        this.load()
+    }
+  },
+  created(){
+      this.getShopData();
+  },
 }
 </script>
 
 <style lang="scss" scoped>
   #commodity{
+    .van-dialog{
+           z-index: 5;
+          //  display: none;
+          .van-dialog__message {
+                text-align:center;
+                padding-right: 80px;
+                box-sizing: border-box;
+                 }
+         }
+     
       .vanList{
           width: 100%;
           height: 115px;
+          .shop{
+                display: flex;
+               .logo{
+                width: 115px;
+                height: 98px;
+                display: flex;
+                background: #F3F3F3;
+                justify-content: center;
+                align-items: center;
+                border-radius: 3px;
+                  img{
+                    width: 80px;
+                    height: 63.3px;
+                  }
+             
+            }
+          }
       }
       .danger{
           height: 115px;
       }
+  }
+  #commodity{
+       .massage{
+                    width:260px;
+                    height: 98px;
+                    padding:10px 0 0 20px;
+                    box-sizing: border-box;
+                      p{
+                        width: 196px;
+                        height: 14px;
+                        line-height: 14px;
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        margin-bottom: 15px;
+                      }
+                      a{
+                        display: block;
+                        margin-bottom: 15px;
+                        font-size: 14px;
+                        color: #FE3824;
+                        line-height: 14px;
+                      }
+                      span{
+                        display: block;
+                        font-size: 12px;
+                        color: #999999;
+                        line-height: 12px;
+                      }
+                   
+                }
+                .cover{
+                      width: 100%;
+                      position: fixed;
+                      top: 0;
+                      height: 100%;
+                      left: 0;
+                      background-color: rgba(0, 0, 0, 0.7);
+                      z-index: 0;
+                    }
   }
 </style>
 
